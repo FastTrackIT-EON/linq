a62b660cdea6d72a98534d26e969ed28a13b472a
Florin Lazar <florin.lazar.it2014@gmail.com>
2024-05-10 10:26:22 +0300

    examples with filters and projection

--- LinqExercises/LinqExercises/Program.cs
+++ LinqExercises/LinqExercises/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 namespace LinqExercises
@@ -8,32 +9,38 @@ namespace LinqExercises
     {
         static void Main(string[] args)
         {
-            Example_1_MyFirstQuery();
-
             PersonsDatabase.ReadFromXmlUsingXmlReader("Persons.xml");
+            ProductsDatabase.ReadFromXml("CategoriesWithProducts.xml");
 
-            foreach (Person p in PersonsDatabase.Persons)
-            {
-                p.Print();
-            }
+            // Example_1_MyFirstQuery();
+            // Example_2_Where_WithPersons();
+            // Example_3_OfType_WithPersons();
+            // Example_3_OfType_WithObjects();
 
-            PersonsDatabase.SaveToXmlUsingXmlWriter("PersonsXmlWriter.xml");
+            Example_5_Select_ProjectDateOfBirths();
+
+            //foreach (Person p in PersonsDatabase.Persons)
+            //{
+            //    p.Print();
+            //}
+
+            //PersonsDatabase.SaveToXmlUsingXmlWriter("PersonsXmlWriter.xml");
 
             Console.WriteLine();
             Console.WriteLine("-------------");
             Console.WriteLine();
 
-            ProductsDatabase.ReadFromXml("CategoriesWithProducts.xml");
+            
 
-            foreach (Category category in ProductsDatabase.Categories)
-            {
-                category.Print();
-            }
+            //foreach (Category category in ProductsDatabase.Categories)
+            //{
+            //    category.Print();
+            //}
 
-            foreach (Product product in ProductsDatabase.Products)
-            {
-                product.Print();
-            }
+            //foreach (Product product in ProductsDatabase.Products)
+            //{
+            //    product.Print();
+            //}
 
             Console.WriteLine("Press any key to close...");
             Console.ReadKey();
@@ -70,5 +77,118 @@ namespace LinqExercises
             Console.WriteLine("Press any key to close...");
             Console.ReadKey();
         }
+
+        private static void Example_2_Where_WithPersons()
+        {
+            //var query = from p in PersonsDatabase.Persons
+            //            where p.Age > 14 && p.FirstName.StartsWith("D", StringComparison.OrdinalIgnoreCase)
+            //            select p;
+
+            var query = PersonsDatabase
+                .Persons
+                .Where((p, index) => p.Age > 14 &&
+                                     p.FirstName.StartsWith("D", StringComparison.OrdinalIgnoreCase));
+
+            foreach (Person p in query)
+            {
+                p.Print();
+            }
+        }
+
+        private static void Example_3_OfType_WithPersons()
+        {
+            // need to have elements of different types
+            // so we use inheritance 
+            PersonsDatabase.Persons.Add(new Student(
+                "StudentFirstName",
+                "StudentLastName",
+                new DateTime(1990, 5, 10),
+                Gender.Female,
+                "University of Cluj"));
+
+            var query = PersonsDatabase.Persons.OfType<Student>();
+
+            foreach (var student in query)
+            {
+                student.Print();
+            }
+        }
+
+        private static void Example_3_OfType_WithObjects()
+        {
+            List<object> elements = new List<object>
+            {
+                1,
+                2,
+                "Test",
+                "Test123",
+                new Person("FirstName", "LastName", new DateTime(1991, 5, 10), Gender.Male),
+                true
+            };
+
+            // var query = elements.OfType<int>().Where(nr => nr % 2 == 0);
+            var query = elements.OfType<DateTime>()
+                                .Where(d => d.Year == 2024)
+                                .Select(d => d.Year.ToString());
+
+            foreach (var result in query)
+            {
+                Console.WriteLine(result);
+                // result.Print();
+            }
+        }
+
+        private static void Example_4_OfType_WithDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add(new DataColumn("Id", typeof(int)));
+            table.Columns.Add(new DataColumn("ProductName", typeof(string)));
+            table.Columns.Add(new DataColumn("Price", typeof(decimal)));
+
+            table.Rows.Add(1, "Product 1", 3.5M);
+            table.Rows.Add(2, "Product 2", 5M);
+            table.Rows.Add(3, "Product 3", 5.5M);
+
+            var query = table.AsEnumerable()
+                .Select(row => row["ProductName"])
+                .OfType<string>()
+                .Where(p => p.EndsWith("2", StringComparison.OrdinalIgnoreCase));
+
+            foreach (var result in query)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+
+        private static void Example_5_Select_ProjectDateOfBirths()
+        {
+         
+            int nrDeOrdine = 0;
+            var query = from p in PersonsDatabase.Persons
+                        where p.Age > 18
+                        select new
+                        {
+                            p.DateOfBirth,
+                            NrDeOrdine = ++nrDeOrdine
+                        };
+
+
+            /*
+            int nrDeOrdine = 0;
+            var query = PersonsDatabase.Persons
+                .Where(p => p.Age > 18)
+                .Select((p, index) => new
+                {
+                    DateOfBirth = p.DateOfBirth,
+                    NrDeOrdine = ++nrDeOrdine
+                });
+            */
+            foreach (var result in query)
+            {
+                Console.WriteLine($"{result.NrDeOrdine} - {result.DateOfBirth:yyyy-MM-dd}");
+            }
+        }
+
     }
 }
---
+++ LinqExercises/LinqExercises/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinqExercises
+{
+    public class Student : Person
+    {
+        public Student(
+            string firstName,
+            string lastName,
+            DateTime dateOfBirth,
+            Gender gender,
+            string university)
+            : base (firstName, lastName, dateOfBirth, gender)
+        { 
+            University = university;
+        }
+
+        public string University { get; }
+    }
+}
